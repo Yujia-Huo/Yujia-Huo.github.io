61023ee972c1d0d7960ae5522bc530b6d59dd2c6
Yujia-Huo <yjhuo@ucdavis.edu>
2023-01-30 18:39:57 -0500

    Visualization Works

--- Tech2/A2_Design/A2_D_Script.js
+++ Tech2/A2_Design/A2_D_Script.js
@@ -14,134 +14,216 @@ let filtered_data = data.filter(function(d) {
         return d.Year === '1900'
     });
 
+let infant_filter = filtered_data.filter(function(d) {
+    return d.Stage === 'Infant'
+});
 
-function draw(a){
+let uadult_filter = filtered_data.filter(function(d) {
+    return d.Stage === 'Under adult'
+});
 
-    var numNodes = a
-var nodes = d3.range(numNodes).map(function(d) {
-	return {radius: 20}
-})
+let adult_filter = filtered_data.filter(function(d) {
+    return d.Stage === 'Adult'
+});
 
-var simulation = d3.forceSimulation(nodes)
-	.force('charge', d3.forceManyBody().strength(5))
-	.force('center', d3.forceCenter(width / 2, height / 2))
-	.force('collision', d3.forceCollide().radius(function(d) {
-		return d.radius
-	}))
-	.on('tick', ticked);
+let senior_filter = filtered_data.filter(function(d) {
+    return d.Stage === 'Senior'
+});
 
-function ticked() {
-	var u = d3.select('svg')
-		.selectAll('circle')
-		.data(nodes)
-		.join('circle')
-		.attr('r', function(d) {
-			return d.radius
-		})
-		.attr('cx', function(d) {
-			return d.x
-		})
-		.attr('cy', function(d) {
-			return d.y
-		})
+infant_sum  = d3.sum(infant_filter, function(d) { return +d.People; })
+uadult_sum  = d3.sum(uadult_filter, function(d) { return +d.People; })
+adult_sum  = d3.sum(adult_filter, function(d) { return +d.People; })
+senior_sum  = d3.sum(senior_filter, function(d) { return +d.People; })
+
+
+
+let filtered_data2 = data.filter(function(d) {
+    return d.Year === '2000'
+});
+
+let infant_filter2 = filtered_data2.filter(function(d) {
+return d.Stage === 'Infant'
+});
+
+let uadult_filter2 = filtered_data2.filter(function(d) {
+return d.Stage === 'Under adult'
+});
+
+let adult_filter2 = filtered_data2.filter(function(d) {
+return d.Stage === 'Adult'
+});
+
+let senior_filter2 = filtered_data2.filter(function(d) {
+return d.Stage === 'Senior'
+});
+
+infant_sum2  = d3.sum(infant_filter2, function(d) { return +d.People; })
+uadult_sum2 = d3.sum(uadult_filter2, function(d) { return +d.People; })
+adult_sum2  = d3.sum(adult_filter2, function(d) { return +d.People; })
+senior_sum2  = d3.sum(senior_filter2, function(d) { return +d.People; })
+
+
+console.log(senior_filter);
+let group1= svg.append('g')
+let group2= svg.append('g')
+let group3= svg.append('g')
+let group4= svg.append('g')
+let group5= svg.append('g')
+let group6= svg.append('g')
+let group7= svg.append('g')
+let group8= svg.append('g');
+// console.log(infant_sum);
+
+
+svg.append('text')
+.attr('x', 1500)
+.attr('y', 220+200)
+.attr('stroke', 'Black')
+.style("font-size", 50)
+.text("1990 and 2000")
+.style("font-weight", 50)
+
+svg.append('text')
+  .attr('x', 1500)
+  .attr('y', 300+200)
+  .attr('stroke', 'Black')
+  .style("font-size", 50)
+  .text("US Population")
+
+  svg.append('text')
+  .attr('x', 1500)
+  .attr('y', 380+200)
+  .attr('stroke', 'Black')
+  .style("font-size", 50)
+  .text("Distribution")
+
+
+svg.append('text')
+  .attr('x', 100)
+  .attr('y', 300)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("1990")
+
+  svg.append('text')
+  .attr('x', 100)
+  .attr('y', 800)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("2000")
+
+  svg.append('text')
+  .attr('x', 300)
+  .attr('y', 100)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("Infant")
+
+  svg.append('text')
+  .attr('x', 300)
+  .attr('y', 150)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("(0-5)")
+
+  svg.append('text')
+  .attr('x', 560)
+  .attr('y', 100)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("Under Adult")
+
+  svg.append('text')
+  .attr('x', 580)
+  .attr('y', 150)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("(10-15)")
+
+  svg.append('text')
+  .attr('x', 910)
+  .attr('y', 100)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("Adult")
+
+  svg.append('text')
+  .attr('x', 900)
+  .attr('y', 150)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("(20-55)")
+
+  svg.append('text')
+  .attr('x', 1200)
+  .attr('y', 100)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("Senior")
+
+  svg.append('text')
+  .attr('x', 1200)
+  .attr('y', 150)
+  .attr('stroke', 'Black')
+  .style("font-size", 30)
+  .text("(60+)")
+
+function draw(){
+    for (let i = 0; i < 9; i++) {
+            console.log(i);
+            var numNodes = [infant_sum, uadult_sum, adult_sum, senior_sum, infant_sum2, uadult_sum2, adult_sum2, senior_sum2]
+            let xp= [200, 500, 800, 1100, 200, 500, 800, 1100]
+            let groups = [group1, group2, group3, group4, group5, group6, group7, group8]
+            let color=[ '#470024','#01847F','#492D22', '#003153','#470024','#01847F','#492D22', '#003153']
+            if(i<4){
+                cy =300;
+            }
+            if(i>3){
+                cy =800;
+            }
+            // var xCenter = [100, 300, 500];
+            var nodes = d3.range(numNodes[i]/300000).map(function(d) {
+            return {radius: 5}
+            })
+            console.log(cy);
+
+            let node = groups[i].selectAll('circle')
+            .data(nodes)
+            .join('circle')
+            .attr('r', function(d) {return d.radius})
+            .attr('cx', function(d) {return xp[i]})
+            .attr('cy', cy)
+            .attr('fill', function(d) {return color[i]})
+            .attr('fill-opacity', "100%")
+            .attr("stroke", "black")
+            .style("stroke-width", 0)
+
+
+            // // Features of the forces applied to the nodes:
+            var simulation = d3.forceSimulation()
+            .force("x", d3.forceX().strength(1.3).x(xp[i]+150))
+            .force("y", d3.forceY().strength(0.1).y( cy ))
+            .force("charge", d3.forceManyBody().strength(8)) // Nodes are attracted one each other of value is > 0
+            .force("collide", d3.forceCollide().strength(.1).radius(10).iterations(1)) // Force that avoids circle overlapping
+
+    // // Apply these forces to the nodes and update their positions.
+    // // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+            simulation
+            .nodes(nodes)
+            .on("tick", function(d){
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y)
+            });
+
+    
+    }
+
 }
 
 
-//     const x = d3.scaleOrdinal()
-//     .domain(["Infant", "Under adult", "Adult", "Senior"])
-//     .range([50, 200, 350, 500])
+draw();
 
-//     // A color scale
-//     const color = d3.scaleOrdinal()
-//     .domain(["Infant", "Under adult", "Adult", "Senior"])
-//     .range([ "#F8766D", "#00BA38", "#619CFF", "Black"])
-
-//     // a= a/2000;
-//     // let data = d3.range(a);
-//     // console.log(data);
-
-//     let node = svg.append("g")
-//     .selectAll("circle")
-//     .data(a)
-//     .join("circle")
-//         .attr("r", 10)
-//         .attr("cx",width /2 )
-//         .attr("cy", height / 2)
-//         .style("fill", "black")
-//         .style("fill-opacity", 0.8)
-//         .attr("stroke", "black")
-//         .style("stroke-width", 4)
-
-
-//         // Features of the forces applied to the nodes:
-//         const simulation = d3.forceSimulation()
-//         // .on("tick", tick)
-//         .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-//         .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
-//         .force("collide", d3.forceCollide().strength(.01).radius(1).iterations(1)) // Force that avoids circle overlapping
-//         .stop();
-
-// // // Apply these forces to the nodes and update their positions.
-// // // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-//         simulation
-//         .nodes(a)
-//         .on("tick", function(d){
-//         node
-//             .attr("cx", d => d.x)
-//             .attr("cy", d => d.y)
-//         });
-    }
-    // const num = [];
-    // num.length = 10;
-
-draw(10);
-// create dummy data -> just one element per circle
-// const data = [{ "name": "A", "group": 1 }, { "name": "B", "group": 1 }, { "name": "C", "group": 1 }, { "name": "D", "group": 1 }, { "name": "E", "group": 1 }, { "name": "F", "group": 1 },
-//             { "name": "G", "group": 2 }, { "name": "H", "group": 2 }, { "name": "I", "group": 2 }, { "name": "J", "group": 2 }, { "name": "K", "group": 2 }, { "name": "L", "group": 2 },
-//             { "name": "M", "group": 3 }, { "name": "N", "group": 3 }, { "name": "O", "group": 3 }]
-
-// A scale that gives a X target position for each group
-
-const pop = {
-        min: d3.min(filtered_data, function(d) { return +d.People; }),
-        max: d3.max(filtered_data, function(d) { return +d.People; })
-};
-
-
-
-console.log(pop);
-
-
-// Initialize the circle: all located at the center of the svg area
-// const node = svg.append("g")
-//   .selectAll("circle")
-//   .data(filtered_data)
-//   .join("circle")
-//     .attr("r", function(d) { return rScale(d.People); })
-//     .attr("cx",width / 2)
-//     .attr("cy", height / 2)
-//     .style("fill", d => color(d.Stage))
-//     .style("fill-opacity", 0.8)
-//     .attr("stroke", "black")
-//     .style("stroke-width", 4)
-
-// // Features of the forces applied to the nodes:
-// var simulation = d3.forceSimulation()
-//     .force("x", d3.forceX().strength(1).x(d => x(d.Stage)))
-//     .force("y", d3.forceY().strength(0.1).y( height/2 ))
-//     .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-//     .force("charge", d3.forceManyBody().strength(10)) // Nodes are attracted one each other of value is > 0
-//     .force("collide", d3.forceCollide().strength(.1).radius(13).iterations(1)) // Force that avoids circle overlapping
-
-// // // Apply these forces to the nodes and update their positions.
-// // // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-// simulation
-//     .nodes(filtered_data)
-//     .on("tick", function(d){
-//       node
-//           .attr("cx", d => d.x)
-//           .attr("cy", d => d.y)
-//     });
 
 
 })
