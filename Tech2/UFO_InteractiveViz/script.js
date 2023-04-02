d180770970cd5bc5a96dac50fe0b07585d159900
Yujia-Huo <yjhuo@ucdavis.edu>
2023-04-01 21:35:20 -0400

    style

--- Tech2/UFO_InteractiveViz/script.js
+++ Tech2/UFO_InteractiveViz/script.js
@@ -36,19 +36,50 @@ function parseCountries(d) {
     }
 }
 }
+
+function parseWord(d){
+    if(d.word != 'I' &&
+        d.word != '((NUFORC' &&
+        d.word != 'lights' &&
+        d.word != 'Light' &&
+        d.word != '2' &&
+        d.word != '3' &&
+        d.word != '4' &&
+        d.word != '5' &&
+        d.word != '((anonymous' &&
+        d.word != 'PD))' &&
+        d.word != 'Note:' &&
+        d.word != 'that' &&
+        d.word != 'i' &&
+        d.word != 'have' &&
+        d.word != 'our' &&
+        d.word != 'what' &&
+        d.word != 'report))' &&
+        d.word != 'other' &&
+        d.word != 'sky,' &&
+        d.word != 'sky.' &&
+        d.word != 'this' &&
+        d.word != 'is'){
+        return{
+            word : d.word, 
+            count: +d.count
+        }
+    }
+}
 //append svg canvas
 
 const plot = d3.select("#map");
-const plot_1 = d3.select('#linechart')
+const plot_1 = d3.select('#linechart');
 const plot2 = d3.select("#shape");
+const plot3 = d3.select('#word');
 
-
+// console.log(plot3)
 //map
 const svg= plot.append("svg")
     // .attr("width", width)
     // .attr("height", height)
     // .attr("transform", `translate(${margin.left},${margin.top})`)
-    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("viewBox", `0 0 ${width} ${height-300}`)
     .attr("preserveAspectRatio", "xMinYMin meet")
     .style("background-color", 'black');
 
@@ -58,7 +89,7 @@ const svg_1 = plot_1.append("svg")
 // .attr("width", width/2+200)
 // .attr("height", height)
 .attr("preserveAspectRatio", "xMinYMin meet")
-.attr("viewBox", `0 0 ${width-500} ${height}`)
+.attr("viewBox", `0 0 ${width-500} ${height-300}`)
 // .attr("transform", `translate(${margin.left},${margin.top})`)
 .style("background-color", 'black');
 
@@ -67,9 +98,16 @@ const svg_1 = plot_1.append("svg")
 const svg2= plot2.append("svg")
     // .attr("width", width)
     // .attr("height", height+50)
-    .attr("viewBox", `0 0 ${width} ${height+100}`)
+    .attr("viewBox", `0 0 ${width} ${height-300}`)
     .attr("preserveAspectRatio", "xMinYMin meet")
     .style("background-color", 'black');
+
+    
+const svg3 = plot3.append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", `0 0 ${width-300} ${height-300}`)
+    .style("background-color", 'black')
+  .append("g")
 
 // const svg2_2 =  plot2.append("svg")
 // .attr("width", width)
@@ -91,10 +129,10 @@ const state = d3.geoPath().projection(projection);
 //import data
 const usaMapPromise = d3.json("./data/USA.json");
 var obsPromise = d3.csv("https://gist.githubusercontent.com/Yujia-Huo/a16c2c58f1e92a46d9055a14953a6406/raw/5caa67713594746217b36eb7480fb87ade883ed3/nuforc_reports.csv",parseCountries);
-
+var wordCountPromise = d3.csv('https://gist.githubusercontent.com/Yujia-Huo/41d3baec9e1397c4725636a20aa03f73/raw/1374ba9b79bcfa3a20173969980a70c64c150205/word_counts.csv', parseWord);
 
 //pass data and generate viz
-Promise.all([usaMapPromise, obsPromise]).then(function([usamap, obs]){
+Promise.all([usaMapPromise, obsPromise, wordCountPromise]).then(function([usamap, obs, wordCount]){
 
 
     /***************************** */
@@ -130,7 +168,7 @@ Promise.all([usaMapPromise, obsPromise]).then(function([usamap, obs]){
             }
         }
 
-        console.log(yearCount);
+        // console.log(yearCount);
         //line chart
 
         //x scale
@@ -181,8 +219,7 @@ Promise.all([usaMapPromise, obsPromise]).then(function([usamap, obs]){
 
     /***************************** */
     // VIZ 2 
-
-
+    // console.log(obs.length);
 
         //sumary all shape type
         const Allshape = obs.map(function(d) {
@@ -232,48 +269,64 @@ Promise.all([usaMapPromise, obsPromise]).then(function([usamap, obs]){
             .enter()
             .append("line")
             .attr("x1", 50)
+            // .attr("x1", function(d) { return xScale(d.total); })
             .attr("x2", xScale(0))
             .attr("y1", function(d) { return yScale(d.shape); })
             .attr("y2", function(d) { return yScale(d.shape); })
             .attr("stroke", "grey")
 
-
         const popCircle = svg2.selectAll("lolipopCircle")
             .data(shapeCount)
             .enter()
             .append("circle")
-                .attr("cx", 50)
+            .attr('cx', 50)
+            // .attr("cx", function(d) { return xScale(d.total); })
                 .attr("cy", function(d) { return yScale(d.shape); })
                 .attr("r", "6")
                 .style("fill", "rgba(165, 241, 250, 0.992)")
                     //   .attr("stroke", "black")
-                    .on("mouseover", function (e, d) {
-                        tooltip.style("visibility", "visible")
-                            .style("left",(e.pageX+50)+"px")
-                            .style("top",(e.pageY)+"px")
-                            .html(`Shape: &nbsp${d.shape} <br> Count: &nbsp${d.total}`);
-            
-                        // console.log(d3.select(this));
-                        d3.select(this)
-                            .transition()
-                            .duration(200)
-                            .delay(1000)
-                            .attr("r", "10");
-                            // .style("fill", 'black');
-            
-                    }).on("mouseout", function () {
-            
-                        tooltip.style("visibility", "hidden");
-            
-                        d3.select(this)
-                        .transition()
-                        .duration(200)
-                        .delay(1000)
-                        .attr("r", "6");
-            
-                    });
 
+        svg2.on('mouseover', function(){
+            popCircle
+            // .interrupt()
+            .transition()
+            .duration(1000)
+            .attr("cx", function(d) { return xScale(d.total); })
+            .end();
+            // .attr("cy", function(d) { return yScale(d.shape); })
+            // .attr("r", "1")
+            // .on('end', function(){})
+            shapeLine
+            .transition()
+            .duration(1000)
+            .attr("x1", function(d) { return xScale(d.total); })
+            .end();
 
+        })
+        popCircle.on("mouseover", function (e, d) {
+            tooltip.style("visibility", "visible")
+                .style("left",(e.pageX+50)+"px")
+                .style("top",(e.pageY)+"px")
+                .html(`Shape: &nbsp${d.shape} <br> Count: &nbsp${d.total}`);
+
+            // console.log(d3.select(this));
+            d3.select(this)
+                .transition()
+                .duration(200)
+                // .delay(1000)
+                .attr("r", "10");
+                // .style("fill", 'black');
+
+        }).on("mouseout", function () {
+
+            tooltip.style("visibility", "hidden");
+
+            d3.select(this)
+            .transition()
+            .duration(200)
+            .attr("r", "6");
+
+        });
         //find max and min of the years
         dateMaxMin = d3.extent(obs, function (d) { return +d.date });
         console.log(dateMaxMin);
@@ -314,21 +367,72 @@ Promise.all([usaMapPromise, obsPromise]).then(function([usamap, obs]){
         // })
 
 
-        svg2.on('mouseover.once', function(){
-            popCircle
-            // .interrupt()
-            .transition()
-            .duration(1000)
-            .attr("cx", function(d) { return xScale(d.total); });
-            // .attr("cy", function(d) { return yScale(d.shape); })
-            // .attr("r", "1")
-            // .on('end', function(){})
-            shapeLine
-            .transition()
-            .duration(1000)
-            .attr("x1", function(d) { return xScale(d.total); });
+/***************************** */
+//viz 3
 
-        })
+        var sortedWordCount = wordCount.sort(function(a,b) { return +b.count - +a.count });
+        var top100WordCount = sortedWordCount.filter(function(d,i){ return i<80 })
+        console.log(top100WordCount);
+
+        var sizeScale = d3.scaleLinear()
+        .domain(d3.extent(top100WordCount, function(d){return d.count; }))
+        .range([8, 50]);
+
+
+        // console.log(sizeScale(top100WordCount.count))
+        console.log(top100WordCount);
+
+        // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+        var layout = d3.layout.cloud()
+        .size([width/2, height/2])
+        .words(top100WordCount.map(function(d) { return {text: d.word, size: d.count}; }))
+        .rotate(0)
+        .padding(5)
+        .fontSize(function(d) { return sizeScale(d.size)})
+        //   .fontSize(20)
+
+        .on("end", draw);
+        layout.start();
+
+
+        // This function takes the output of 'layout' above and draw the words
+        // Better not to touch it. To change parameters, play with the 'layout' variable above
+        function draw(words) {
+                var opacityScale = d3.scaleLinear()
+                .domain(d3.extent(words, function(d){return d.size ; }))
+                .range([0.2, 1]);
+                svg3
+                .append("g")
+                .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+                .selectAll("text")
+                    .data(words)
+                    .enter().append("text")
+                    .style("font-size", function(d) { return d.size; })
+                    .attr("text-anchor", "middle")
+                    .attr('fill', 'white')
+                    // .style('opacity',0)
+                    .style('opacity',function(d){ return opacityScale(d.size); })
+                    .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function(d) { return d.text; })
+                    .on('mouseover', function(e, d){
+                        d3.select(this)
+                        .transition()
+                        .duration(200)
+                        .attr('fill', 'rgba(165, 241, 250, 1)')
+                        .style('opacity',1)
+
+                    })
+                    .on('mouseout', function(e, d){
+                        d3.select(this)
+                        .transition()
+                        .duration(200)
+                        .attr('fill', 'white')
+                        .style('opacity',function(d){ return opacityScale(d.size); })
+
+                    })
+            }
 
      /***************************** */
     // A FUNCTION CONTAINS DRAW CIRCLE ON MAP AND HIGHLIGHT ON LINE CHART FOR EACH SELECTED YEAR BY THE SLIDER
@@ -338,10 +442,10 @@ Promise.all([usaMapPromise, obsPromise]).then(function([usamap, obs]){
         // filter data of selected year
         filteredDate = obs.filter(function(d){ return d.date == year });
 
-        console.log('year'+ year)
+        // console.log('year'+ year)
         //find count value of selected year
         var count = d3.format(',')(filteredDate.length);
-        console.log(count);
+        // console.log(count);
 
 
        //draw circle on map
@@ -438,23 +542,6 @@ Promise.all([usaMapPromise, obsPromise]).then(function([usamap, obs]){
     d3.select(".track")
     .attr("stroke-width", "1px")
     .style("background-color", "gray");
-
-    console.log(d3.select(".parameter-value"));
-    // .transform('translate(830)')
-    // Animate the slider handle from left to right automatically
-    //draw new
-//     for(i=0;i<53;i++){
-//     svg.selectAll("circle").remove();
-//     svg_1.selectAll('.discription').remove();
-//     svg_1.selectAll("line").transition().duration(200).style("opacity", 0).remove();
-//     svg_1.selectAll("circle").transition().duration(200).style("opacity", 0);
-//     updateDate(1968+i);
-//     //show circle on the selected year
-//     // svg_1.selectAll(".y"+1968).transition().duration(800)..delay(300).style("opacity", 1);
-// }
-
-    // console.log(d3.select('#value').text());
-    //event listener
 
 sliderCanvas;
     }
